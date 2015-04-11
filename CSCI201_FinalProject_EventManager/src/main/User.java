3540156ca3e683d263bb8b5f4568409116b67e31
John Pugliesi <jpugliesi12@gmail.com>
2015-04-11 14:33:10 -0700

    Update Event and User classes with new getters/setters and constructors to match database structure

--- CSCI201_FinalProject_EventManager/src/main/User.java
+++ CSCI201_FinalProject_EventManager/src/main/User.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.Vector;
 
 public class User implements Serializable{
@@ -12,14 +13,13 @@ public class User implements Serializable{
 	private boolean isAdmin;
 	private Vector<Event> eventVector;
 	
-	public User(int userID, int profilePictureID, String fullName, String userName, String password, boolean isAdmin, Vector<Event> eventVector){
-		this.userID = userID;
-		this.profilePictureID = profilePictureID;
+	public User(String fullName, String userName, String password, boolean isAdmin, int profilePictureID){
 		this.fullName = fullName;
 		this.userName = userName;
 		this.password = password;
 		this.isAdmin = isAdmin;
-		this.eventVector = eventVector;
+		this.profilePictureID = profilePictureID;
+		this.eventVector = new Vector<Event>();
 	}
 	
 	public User(){
@@ -37,10 +37,38 @@ public class User implements Serializable{
 	public String getPassword(){
 		return password;
 	}
+	
+	public String getFullName(){
+		return fullName;
+	}
+	
+	public boolean isAdmin(){
+		return isAdmin;
+	}
+	
+	public int getProfilePictureID(){
+		return profilePictureID;
+	}
+	
+	public int getUserID(){
+		return userID;
+	}
+	
+	public void setUserID(int id){
+		this.userID = id;
+	}
+	
+	public void addEvent(Event e){
+		this.eventVector.add(e);
+	}
+	
+	public void setEvents(Vector<Event> events){
+		this.eventVector = events;
+	}
 
 	public void testAttendedEvents() {
-		this.eventVector.add(new Event(0,1, "Event 4", "VKC 201", "12:00pm", "Club Meeting!"));
-		this.eventVector.add(new Event(0,2, "Event 5", "Leavy Library", "9:30pm", "Study Club!"));
+		this.addEvent(new Event("Event 4", "VKC 201", new Date(System.currentTimeMillis()), "ACM", "Club Meeting!", 10, 1));
+		this.addEvent(new Event("Event 5", "Leavy Library", new Date(System.currentTimeMillis()), "SAS", "Study Club!", 18, 1));
 		
 	}
 
