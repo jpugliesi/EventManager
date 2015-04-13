33cc99b8546ad8b8a584d06c8097c0af293fdb2f
Kieran Stolorz <stolorz@usc.edu>
2015-04-12 17:36:05 -0700

    getters and setters for chat history

--- CSCI201_FinalProject_EventManager/src/main/ChatMessage.java
+++ CSCI201_FinalProject_EventManager/src/main/ChatMessage.java
@@ -5,18 +5,47 @@ import java.util.Date;
 
 public class ChatMessage {
 	private String message;
-	private User sender;
+	private User sender, receiver;
 	private Date date;
 	
 	
-	public ChatMessage(String message, User sender, Date date){
+	public ChatMessage(String message, User sender, User receiver,  Date date){
 		this.message = message;
 		this.sender = sender;
+		this.receiver = receiver;
 		this.date = date;
 	}
 	
-	String getMessage(){
+	public String getMessage(){
 		return message;
+	}
+	
+	public User getSender(){
+		return sender;
+	}
+	
+	public User getReceiver(){
+		return receiver;
+	}
+	
+	public Date getDate(){
+		return date;
+	}
+	
+	public void setMessage(String m){
+		message = m;
+	}
+	
+	public void setSender(User u){
+		sender = u;
+	}
+	
+	public void setReceiver(User u){
+		receiver = u;
+	}
+	
+	public void setDate(Date d){
+		date = d;
 	}
 	
 	
