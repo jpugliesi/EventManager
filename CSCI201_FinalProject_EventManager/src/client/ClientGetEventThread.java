5c0dfdb567a83a5a462829420c8b387ebef59d33
Vincent Jin <chenjin@usc.edu>
2015-04-17 01:02:54 -0700

    adding client thread

--- CSCI201_FinalProject_EventManager/src/client/ClientGetEventThread.java
+++ CSCI201_FinalProject_EventManager/src/client/ClientGetEventThread.java
@@ -1,5 +1,59 @@
 package client;
 
-public class ClientGetEventThread {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
+import main.Event;
+import constants.Constants;
+
+public class ClientGetEventThread extends Thread{
+	private Socket socket;
+	private ObjectInputStream inputStream;
+	private ObjectOutputStream outputStream;
+	private Integer id;
+	private Event e;
+	
+	public ClientGetEventThread(Integer id, Event e) {
+		this.id = id;
+		this.e = e;
+	}
+	
+	public void run() {
+		try {
+			socket = new Socket(Constants.SERVER_IP, 6789);
+			
+			//setup input and output stream
+			outputStream = new ObjectOutputStream(socket.getOutputStream());
+			inputStream = new ObjectInputStream(socket.getInputStream());
+
+			outputStream.writeObject(Constants.CLIENT_GET_EVENT);
+			outputStream.flush();
+			
+			outputStream.writeObject(id);
+			outputStream.flush();
+			
+			outputStream.writeObject(e);
+			outputStream.flush();
+			
+			int code = (Integer) inputStream.readObject();
+			//success case
+			if (code == Constants.SERVER_GET_EVENT_SUCCESS) {
+				System.out.println("Success get detail event");
+				//TODO
+				//direct GUI to the detailed page 
+			}
+			//fail case
+			else if (code == Constants.SERVER_GET_EVENT_FAIL) {
+				System.out.println("Fail get detail event");
+			}
+			
+			
+		} catch (IOException ioe) {
+			System.out.println(ioe.getMessage());
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
