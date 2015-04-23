07273f563292ce4f4593a3e70881c7e546aaf825
John Pugliesi <jpugliesi12@gmail.com>
2015-04-22 22:10:05 -0700

    Fix user tab frame

--- CSCI201_FinalProject_EventManager/src/gui/UserTabFrame.java
+++ CSCI201_FinalProject_EventManager/src/gui/UserTabFrame.java
@@ -2,14 +2,11 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
-<<<<<<< Updated upstream
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-=======
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
->>>>>>> Stashed changes
 import java.util.Vector;
 
 import javax.swing.BoxLayout;
