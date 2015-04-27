6371a2d6c15545ec05b25a64d4a9805b9afc3ad6
Kieran Stolorz <kieranstolorz12@gmail.com>
2015-04-26 21:36:12 -0700

    manage event dialog skeleton

--- CSCI201_FinalProject_EventManager/src/gui/ManageEventDialog.java
+++ CSCI201_FinalProject_EventManager/src/gui/ManageEventDialog.java
@@ -1,15 +1,24 @@
 package gui;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JDialog;
+import javax.swing.JList;
 import javax.swing.JPanel;
+
+import main.Event;
 
 public class ManageEventDialog extends JDialog {
 	
 	private final JPanel mainPanel = new JPanel();
 	private UserEventFeedPanel eventList;
+	private JList<Event> eventJList;
+	private DefaultListModel<Event> listModel;
 	
 	
 	
+	public ManageEventDialog(){
+		
+	}
 	
 
 }
